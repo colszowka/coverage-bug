e7530de19f473d8b793201a48c19334a36c19eed
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-14 23:52:35 +0200

    validates competition end date >= start date

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,11 @@
 class Competition < ActiveRecord::Base
   validates :name, :description, :starts_at, :ends_at, :presence => true
+  validate :ends_at_cannot_be_before_starts_at
+
+private
+  def ends_at_cannot_be_before_starts_at
+    unless starts_at.nil? or ends_at.nil?
+      errors.add(:ends_at, "can't be prior to start date") if ends_at < starts_at
+    end
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -6,5 +6,11 @@ describe Competition do
     it { should validate_presence_of :description }
     it { should validate_presence_of :starts_at }
     it { should validate_presence_of :ends_at }
+
+    it "should require ends_at >= starts_at" do
+      c = FactoryGirl.build(:competition, :starts_at => Date.new(2010, 1, 2), :ends_at => Date.new(2010, 1, 1))
+      c.should_not be_valid
+      c.errors[:ends_at].should_not be_empty
+    end
   end
 end
