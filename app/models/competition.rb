b3c06db0376071257c4b2a37d7c931a3a3e2b776
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-15 01:32:28 +0200

    made custom validation more verbose

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,10 +2,10 @@ class Competition < ActiveRecord::Base
   has_many :news
 
   validates :name, :description, :starts_at, :ends_at, :presence => true
-  validate :ends_at_cannot_be_before_starts_at
+  validate :cannot_end_before_it_started
 
 private
-  def ends_at_cannot_be_before_starts_at
+  def cannot_end_before_it_started
     unless starts_at.nil? or ends_at.nil?
       errors.add(:ends_at, "can't be prior to start date") if ends_at < starts_at
     end
