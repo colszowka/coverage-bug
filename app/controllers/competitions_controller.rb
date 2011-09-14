e044ae0ea0721d4e2e02980cb1a65098a76e1f45
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-14 23:39:09 +0200

    added competitions#new and competitions#create

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,4 +2,17 @@ class CompetitionsController < ApplicationController
   def index
     @competitions = Competition.all
   end
+
+  def new
+    @competition = Competition.new
+  end
+
+  def create
+    @competition = Competition.new params[:competition]
+    if @competition.save
+      redirect_to competitions_path
+    else
+      render :new
+    end
+  end
 end
--- spec/requests/competitions_spec.rb
+++ spec/requests/competitions_spec.rb
@@ -6,4 +6,14 @@ describe "Competition" do
       page.should have_content("Blub")
     end
   end
+
+  describe "POST /competitions" do
+    it "should create competitions" do
+      visit new_competition_path
+      fill_in "Name", :with => "Foo"
+      fill_in "Description", :with => "Muh"
+      click_button "Create Competition"
+      page.should have_content("Foo")
+    end
+  end
 end
