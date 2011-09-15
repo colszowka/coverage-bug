edef6e6ab66baf5984172f1aaed2cfac5df584eb
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-15 04:09:01 +0200

    fix for broken request specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+locale_prefix =
+  if Rails.env.test?
+    ""
+  else
+    "/(:locale)"
+  end
+
 WcaCompetitions::Application.routes.draw do
-  scope "/(:locale)", :locale => /de|en/ do
+  scope locale_prefix, :locale => /de|en/ do
     resources :competitions do
       resources :news
     end
